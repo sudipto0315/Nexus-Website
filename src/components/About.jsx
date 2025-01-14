11e48c61568af3923513281cc4b3c8ae78d2fd8a
Sudipto Ray <sudiptoray2003@gmail.com>
2025-01-14 14:39:00 +0530

    test commit

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -39,7 +39,7 @@ const About = () => {
         />
 
         <div className="about-subtext">
-          <p>The Game of Games begins—your life, now an epic MMORPG</p>
+          <p>The Game of Games that begins—your life, now an epic MMORPG</p>
           <p className="text-gray-500">
             Zentry unites every player from countless games and platforms, both
             digital and physical, into a unified Play Economy
